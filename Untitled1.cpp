3749b503a29b443f6feeab920d630daa352fe74d
Abdunnafi25 <68435705+Abdunnafi25@users.noreply.github.com>
2021-09-26 23:40:34 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,120 @@
+#include<stdio.h>
+
+
+int main (){
+	int jenis,ciri,ketebalan;
+	printf(" ----- Expert System Penentu usia Fosil Purba -----\n");
+	printf(" -------------- Pilih Jenis Tanah -----------------\n");
+	printf("1. Lempung\n");
+	printf("2. Gamping\n");
+	printf("3. Pasir\n");
+	
+	scanf("%d", &jenis);
+	
+	if(jenis==1){
+		printf("----------Pilih Ciri-ciri Tanah---------------\n");
+		printf("1. Lempung Warna biru, Endapan air laut\n");
+		printf("2. Lempung Warna hitam, Endapan lahar dingin(breksi vulkanik)\n");
+		
+		scanf("%d", &ciri);
+		if(ciri == 1){
+			printf("----------Pilih Ketebalan Tanah---------------\n");
+			printf("1. Mencapai 107 meter\n");
+			printf("2. Mencapai 4 - 7 meter\n");
+			printf("3. Mencapai 1 - 2,5  meter\n");
+			printf("4. Mencapai 1 meter\n");
+			
+			scanf("%d", &ketebalan);
+			if(ketebalan == 1){
+				printf("Umur fosil 3.000.000 Tahun\n");
+			}
+			else if(ketebalan==2){
+				printf("Umur fosil 2.600.000 Tahun\n");
+			}
+			else if(ketebalan==3){
+				printf("Umur fosil 2.200.000 Tahun\n");
+			}
+			else if(ketebalan == 4){
+				printf("Umur fosil 1.800.000 Tahun\n");
+			}
+			else {
+				printf("Pilihan salah\n");
+			}
+		}else if(ciri==2){
+			printf("----------Pilih Ketebalan Tanah---------------\n");
+			printf("1. Mencapai 0.7 - 50 meter\n");
+			printf("2. Mencapai 100 meter\n");
+			
+			
+			scanf("%d", &ketebalan);
+			if(ketebalan == 1){
+				printf("Umur fosil 1.700.000 Tahun\n");
+			}
+			else if(ketebalan == 2){
+				printf("Umur fosil 800.000 Tahun\n");
+			}
+			else {
+				printf("Pilihan salah\n");
+			}
+		}else {
+			printf("pilihan salah\n");
+		}
+	}
+	else if(jenis == 2){
+		printf("Umur fosil 730.000 Tahun\n");
+	}
+	else if(jenis ==3){
+		printf("----------Pilih Ciri-ciri Tanah---------------\n");
+		printf("1. Pasir silang siur atau berlumpur, gravel air sungai\n");
+		printf("2. Pasir terdapat batu pumisan, tufa\n");
+		
+		scanf("%d", &ciri);
+		if(ciri == 1){
+			printf("----------Pilih Ketebalan Tanah---------------\n");
+			printf("1. Mencapai 6 - 20 meter\n");
+			printf("2. Mencapai 5.8 - 20 meter\n");
+			printf("3. Mencapai 3 - 16  meter\n");
+			
+			
+			scanf("%d", &ketebalan);
+			if(ketebalan == 1){
+				printf("Umur fosil 720.000 Tahun\n");
+			}
+			else if(ketebalan==2){
+				printf("Umur fosil 480.000 Tahun\n");
+			}
+			else if(ketebalan==3){
+				printf("Umur fosil 250.000 Tahun\n");
+			}
+			else {
+				printf("Pilihan salah\n");
+			}
+		}
+		else if(ciri == 2){
+			printf("----------Pilih Ketebalan Tanah---------------\n");
+			printf("1. Mencapai 2.8 - 28.9 meter\n");
+			printf("2. Mencapai 20 meter\n");
+			printf("3. Mencapai 25  meter\n");
+			
+			
+			scanf("%d", &ketebalan);
+			if(ketebalan == 1){
+				printf("Umur fosil 240.000 Tahun\n");
+			}
+			else if(ketebalan==2){
+				printf("Umur fosil 120.000 Tahun\n");
+			}
+			else if(ketebalan==3){
+				printf("Umur fosil 11.000 Tahun\n");
+			}
+			else {
+				printf("Pilihan salah\n");
+			}
+		}
+		else {
+			printf("Pilihan salah");
+		}
+	}
+	
+
+}
